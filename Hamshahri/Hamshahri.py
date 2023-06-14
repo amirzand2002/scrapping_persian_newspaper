85be1e5a079b4c10d65fbdb26ad025a8483b1ca0
amir zand <amirzand2002@gmail.com>
2023-06-14 15:34:11 +0330

    reading all archive

--- Hamshahri/Hamshahri.py
+++ Hamshahri/Hamshahri.py
@@ -2,10 +2,12 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 import re
-import pymongo
+from tqdm import tqdm
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0'}
+
+
 def get_f_news_id():
     # get the oldest issue number and link
     url = 'https://newspaper.hamshahrionline.ir/archive'
@@ -13,14 +15,15 @@ def get_f_news_id():
     data = BeautifulSoup(response.text, 'html.parser')
     news_data = data.find_all('li', attrs={'class', 'page-item'})[-2]
     per_date_archive = news_data.find('a').get('href')
-    url_per_date_archive = 'https://newspaper.hamshahrionline.ir'+per_date_archive
+    url_per_date_archive = 'https://newspaper.hamshahrionline.ir' + per_date_archive
     response_per_date_archive = requests.request("GET", url=url_per_date_archive, headers=headers)
     data_per_date_archive = BeautifulSoup(response_per_date_archive.text, 'html.parser')
     news_data_per_date_archive = data_per_date_archive.find_all('div', attrs={'class', 'col-md-4'})[-1]
     news_data_per_date_archive = news_data_per_date_archive.find('a').get('href')
-    first_news_id= news_data_per_date_archive.replace('/?n=','')
-    first_news_id =int(first_news_id)
+    first_news_id = news_data_per_date_archive.replace('/?n=', '')
+    first_news_id = int(first_news_id)
     return first_news_id
+
 
 def get_lat_news_id():
     # get the latest issue number and link
@@ -29,29 +32,48 @@ def get_lat_news_id():
     data = BeautifulSoup(response.text, 'html.parser')
     news_link = data.find('div', attrs={'class', 'col-md-4'})
     news_link = news_link.find('a').get('href')
-    latest_news_id= news_link.replace('/?n=','')
+    latest_news_id = news_link.replace('/?n=', '')
     latest_news_id = int(latest_news_id)
     return latest_news_id
 
 
 scarped_data = []
-
 latest_news_id = get_lat_news_id()
-first_news_id = get_f_news_id()
-while first_news_id < latest_news_id:
-    url = 'https://newspaper.hamshahrionline.ir/?n=' + str(first_news_id)
+old_news_id = get_f_news_id()
+while old_news_id <= latest_news_id:
+    print(latest_news_id-old_news_id)
+    url = 'https://newspaper.hamshahrionline.ir/?n=' + str(old_news_id)
+    old_news_id += 1
     response = requests.request("GET", url=url, headers=headers)
     data = BeautifulSoup(response.text, 'html.parser')
-    date = data.find_all('li', attrs={'class', 'dir-left'})
-    news_data = data.find_all('div',attrs={'class','post'})
-    for news in news_data:
+    date_element = data.find('li', class_='dir-left')
+    date = date_element.get_text(strip=True)
+    links = data.find_all(re.compile("^h[1-6]$"))
+    for i in links:
         news_detail = {}
-        #news_detail['news_title'] = news.title
-        news_detail['news_text'] = news.text        
-        for d in date:
-            news_detail['news_date'] = d.text
+        new_page = i.findChild("a")['href']
+        response = requests.request("GET", url=new_page, headers=headers)
+        data = BeautifulSoup(response.text, 'html.parser')
+        title = data.find_all(re.compile("^h[1-6]$"), attrs={'class', 'title'})
+        news_data = data.find_all('div', attrs={'class', 'text'})
+        for y in title:
+            news_detail['title'] = y.text
+        for x in news_data:
+            news_detail['text'] = x.text
+
+        date_element = data.find('span', id='publishdate')
+        news_detail['per_date'] = date_element.get_text(strip=True)
+        # news_detail['en_date'] = date
+        # time_element = data.find('span', id='publishtime')
+        # ['time'] = time_element.get_text(strip=True)
+
+        # writer_element = data.find('div', string='نویسنده :')
+        # news_detail['writer'] = writer_element.next_sibling.strip()
+
+        # code_element = data.find('div', string='کد مطلب :')
+        # news_detail['code'] = code_element.next_sibling.strip()
+
         scarped_data.append(news_detail)
-    first_news_id += 1
-    print(first_news_id)
 dataframe = pd.DataFrame.from_dict(scarped_data)
-dataframe.to_csv('hamshahrionline.csv', index=False,encoding="utf-8")
+dataframe.to_csv('hamshahrionline.csv', index=False, encoding="utf-8")
+
